43670c26dee02a15c846ac9f1bb15d7105c75022
0xwaleedh <waleedhesham2001@gmail.com>
2024-03-07 00:15:28 +0200

    Merged the keycloak authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Reachware Studio",
-  description: "The ultimate studio for the Reachware integration platform",
-  icons: ["/favicon.ico"]
-};
+// export const metadata: Metadata = {
+//   title: "Reachware Studio",
+//   description: "The ultimate studio for the Reachware integration platform",
+//   icons: ["/favicon.ico"]
+// };
 
 export default function RootLayout({
   children,
@@ -17,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
--- src/components/Header/UserMenu.js
+++ src/components/Header/UserMenu.js
@@ -1,15 +1,20 @@
 import React from "react"
 
+import { useSession, signOut } from "next-auth/react";
 import { Dropdown, NavItem, NavLink } from "react-bootstrap"
 
 import Link from "next/link"
 import Avatar from "../Avatar"
 
 export default function UserMenu() {
+  const { data: session } = useSession();
+
+  const userName = session?.profile.name;
+  
   return (
     <Dropdown as={NavItem} className="ms-auto" align="end">
       <Dropdown.Toggle as={NavLink} className="pe-0" id="userInfo">
-        <Avatar image="/img/avatar-6.jpg" alt="Jason Doe" border priority />
+        <Avatar image="/img/avatar-6.jpg" alt={userName} border priority />
       </Dropdown.Toggle>
       <Dropdown.Menu
         className="dropdown-menu-animated"
@@ -17,14 +22,14 @@ export default function UserMenu() {
         data-bs-popper="none"
       >
         <Dropdown.Header className="text-gray-700">
-          <h6 className="text-uppercase font-weight-bold">Mark Stephen</h6>
+          <h6 className="text-uppercase font-weight-bold">{userName}</h6>
           <small>Web Developer</small>
         </Dropdown.Header>
         <Dropdown.Divider />
         <Dropdown.Item href="#">Settings</Dropdown.Item>
         <Dropdown.Item href="#">Activity log </Dropdown.Item>
         <Dropdown.Divider />
-        <Link href="/login" passHref>
+        <Link href="/login" passHref onClick={() => signOut({ callbackUrl: "/api/auth/logout" })}>
           <Dropdown.Item>Logout</Dropdown.Item>
         </Link>
       </Dropdown.Menu>
