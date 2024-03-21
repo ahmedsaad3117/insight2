7a70fa886812f556ccc5367874ef18b5063b0ea9
0xwaleedh <waleedhesham2001@gmail.com>
2024-03-21 14:18:58 +0200

    Test Callback Issue #13

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,38 +1,30 @@
-import { providers, signIn, getSession, csrfToken } from "next-auth/client";
+// import { providers, signIn, getSession, csrfToken } from "next-auth/client";
+import React from "react";
 
 function signin({ providers }) {
   return (
     <div>
-      {Object.values(providers).map((provider) => {
-        return (
-          <div key={provider.name}>
-            <h1>Custom SignIn Page</h1>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        );
-      })}
+      <h1>Custom SignIn Page</h1>
     </div>
   );
 }
 
 export default signin;
 
-export async function getServerSideProps(context) {
-  const { req } = context;
-  const session = await getSession({ req });
+// export async function getServerSideProps(context) {
+//   const { req } = context;
+//   const session = await getSession({ req });
 
-  if (session) {
-    return {
-      redirect: { destination: "/" },
-    };
-  }
+//   if (session) {
+//     return {
+//       redirect: { destination: "/" },
+//     };
+//   }
 
-  return {
-    props: {
-      providers: await providers(context),
-      csrfToken: await csrfToken(context),
-    },
-  };
-}
+//   return {
+//     props: {
+//       providers: await providers(context),
+//       csrfToken: await csrfToken(context),
+//     },
+//   };
+// }
